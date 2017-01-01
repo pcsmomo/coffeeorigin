235090b67a9502433b74fa733b2fb21e61480333
NoahKim <dongwoo_333@hanmail.net>
2017-01-01 11:35:22 +1100

    install axios, reduxThunk and apply it

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
+import reduxThunk from "redux-thunk";
 
 import App from "./components/App";
+import reducers from "./reducers";
 
 import registerServiceWorker from "./registerServiceWorker";
 
-const store = createStore(() => [], {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
   <Provider store={store}>
