6297e20c634fd3d170c296db89ff3e371e2a6cc2
NoahKim <dongwoo_333@hanmail.net>
2018-02-22 12:45:09 +1100

    Routing in Production and adding in a Heroic Build Step

--- index.js
+++ index.js
@@ -7,9 +7,14 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 4040;
 app.listen(PORT);
