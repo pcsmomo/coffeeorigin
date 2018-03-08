e3b941979ec3f6ae6c5d2322113dabc98f15c2bc
NoahKim <dongwoo_333@hanmail.net>
2018-03-08 15:23:21 +1100

    basic css

--- client/src/components/coffee/origin.js
+++ client/src/components/coffee/origin.js
@@ -6,12 +6,13 @@ class Origin extends Component {
       <div className="coffeeInfo">
         <div className="firstDiv">
           <h3 className="name">{this.props.origin.name}</h3>
-          <div className="map">map</div>
+          <div className="map" />
         </div>
         <div className="secondDiv">
-          <div className="flavorNote">{this.props.origin.flavorNote}</div>
+          <div className="flavorNote">{this.props.origin.flavorNotes}</div>
           <div className="use">{this.props.origin.use}</div>
           <div className="process">{this.props.origin.process}</div>
+          <div className="roastery">{this.props.origin.roastery}</div>
         </div>
       </div>
     );
