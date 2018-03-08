b1694f9e9c0755144050d87d3af9adae597f286b
NoahKim <dongwoo_333@hanmail.net>
2018-03-09 07:57:19 +1100

    jvectormap added

--- client/src/components/coffee/origin.js
+++ client/src/components/coffee/origin.js
@@ -1,12 +1,43 @@
 import React, { Component } from "react";
+import ReactDom from "react-dom";
 
 class Origin extends Component {
+  componentDidMount() {
+    // jQuery problem solved.
+    // https://github.com/facebook/create-react-app/blob/master/packages/react-scripts/template/README.md#using-global-variables
+    const $ = window.$;
+    const el = ReactDom.findDOMNode(this.display);
+    $(el)
+      .vectorMap({
+        map: "world_mill_en",
+        series: {
+          regions: [
+            {
+              scale: {
+                red: "#ff0000"
+              },
+              attribute: "fill",
+              values: {
+                NI: "red"
+              }
+            }
+          ]
+        }
+      })
+      .vectorMap("set", "focus", {
+        scale: 4,
+        lat: 12.865416,
+        lng: -85.207229,
+        animate: true
+      });
+  }
+
   render() {
     return (
       <div className="coffeeInfo">
         <div className="firstDiv">
           <h3 className="name">{this.props.origin.name}</h3>
-          <div className="map" />
+          <div ref={display => (this.display = display)} className="map" />
         </div>
         <div className="secondDiv">
           <div className="flavorNote">{this.props.origin.flavorNotes}</div>
