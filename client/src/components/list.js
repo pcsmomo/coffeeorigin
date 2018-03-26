98f697538c8040e3162e7cd20cad91a598b9501b
NoahKim <dongwoo_333@hanmail.net>
2018-03-26 17:35:47 +1100

    auto calculating animation duration

--- client/src/components/list.js
+++ client/src/components/list.js
@@ -24,11 +24,95 @@ const reArrange = () => {
   toggle = !toggle;
 };
 
+const getKeyframesRule = ruleName => {
+  let ss = document.styleSheets,
+    rule,
+    i,
+    x;
+
+  for (i = 0; i < ss.length; ++i) {
+    if (ss[i].cssRules) {
+      // loop through all the rules
+      for (x = 0; x < ss[i].cssRules.length; ++x) {
+        rule = ss[i].cssRules[x];
+        if (
+          // (rule.type === window.CSSRule.KEYFRAMES_RULE ||
+          //   rule.type === window.CSSRule.WEBKIT_KEYFRAMES_RULE) &&
+          rule.type === window.CSSRule.KEYFRAMES_RULE &&
+          rule.name === ruleName &&
+          rule.cssText.startsWith("@keyframes")
+        ) {
+          return rule;
+        }
+      }
+    }
+  }
+  return null;
+};
+
+const initiateKeyFrame = () => {
+  let marginTop = 735; // same as the height of a scene
+
+  let keyFrameSlide = getKeyframesRule("slide");
+
+  if (keyFrameSlide !== null) {
+    // delete all rules on the keyframe
+    for (let i = 0; i <= 100; i++) keyFrameSlide.deleteRule(`${i}%`);
+
+    // declare variables
+    let sceneList = document.getElementsByClassName("scene");
+    let listLen = sceneList.length;
+    let curPercentage = 0;
+    let curMarginTop = 0;
+
+    let intervalPct = Math.floor(100 / listLen); // 100 % / length
+    let transitionPct = Math.floor(intervalPct * 0.2); // transition time is 1/5
+
+    // for loop until before the last loop
+    for (let i = 0; i < listLen - 1; i++) {
+      keyFrameSlide.appendRule(
+        `${curPercentage}% {margin-top: ${curMarginTop}px; }`
+      );
+      curPercentage += intervalPct;
+      keyFrameSlide.appendRule(
+        `${curPercentage - transitionPct}% {margin-top: ${curMarginTop}px; }`
+      );
+      curMarginTop -= marginTop;
+    }
+
+    // last percentage to make 100%
+    keyFrameSlide.appendRule(
+      `${curPercentage}% {margin-top: ${curMarginTop}px; }`
+    );
+    curPercentage += intervalPct;
+    keyFrameSlide.appendRule(`100% {margin-top: ${curMarginTop}px; }`);
+
+    // console.log(keyFrameSlide.cssText);
+  }
+};
+
+const getInterval = second => {
+  let sceneList = document.getElementsByClassName("scene");
+  console.log(sceneList.length);
+  return sceneList.length * second; // scenes * second
+};
+
+const setAnimationTime = interval => {
+  let listElm = document.getElementById("list");
+  listElm.style.animation = `slide ${interval}s infinite alternate`;
+};
+
 class List extends Component {
   componentDidMount() {
     this.props.fetchCountries(); // * 비동기의 문제로, fetchCountries 가 끝나기 전에 findCountry가 실행된 경우 오류 발생.
     this.props.fetchOrigins();
-    setInterval(reArrange, 13000);
+
+    setTimeout(() => {
+      let interval = getInterval(6); // seconds for a scene.
+      setInterval(reArrange, interval * 1000);
+      setAnimationTime(interval);
+      initiateKeyFrame();
+    }, 300); // Not working because
   }
 
   findCountry(ccode) {
@@ -52,6 +136,8 @@ class List extends Component {
   render() {
     return (
       <div id="list">
+        {this.renderOrigins()}
+        <Blend />
         {this.renderOrigins()}
         <Blend />
       </div>
